ebdfa9767e5cefccf3d711e0b71ff9f632f3aef5
DhanrajjChavan <dhanarajchavan777@gmail.com>
2024-10-04 01:05:35 +0530

    Create 1)Even or Odd.cpp

---
+++ 1)Even or Odd.cpp
@@ -0,0 +1,21 @@
+//Dhanraj Chavan
+//24070123502
+//ENTC A3
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int a;
+    cout<<"Enter the number: ";
+    cin>>a;
+
+    if(a%2 == 0)
+    {
+        cout<<"It is an even number.";
+    }
+    else
+    {
+        cout<<"It is an odd number.";
+    }
+}
